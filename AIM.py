62b4feae28dcf52c3ac9c5a608b6eb317fe39c37
MJLMills <mjohnmills@gmail.com>
2015-02-08 10:56:52 -0800

    more fixes to AIM.py

--- AIM.py
+++ AIM.py
@@ -9,27 +9,35 @@ class CriticalPoint():
         self.signature = signature
         self.position = position
 
+    def printOut(self):
+        print('RANK:      ' + self.rank)
+        print('SIGNATURE: ' + self.signature)
+
 def readTopology(filepath):
     #given an open topology file create all the corresponding python objects
     topologyTree = ET.parse(filepath)
     topologyRoot = topologyTree.getroot()
     if topologyRoot.tag != 'topology':
+        print('Not a Topology File')
+        exit(1)
 
-        for topologicalObject in topologyRoot:
+    for topologicalObject in topologyRoot:
 
-            if topologicalObject.tag == 'CP':
-                #add a CP to the scene with the appropriate data
-                rank = topologicalObject.find(rank).text
-                signature = topologicalObject.find(signature).text
-                x = topologicalObject.find(x).text
-                y = topologicalObject.find(y).text
-                z = topologicalObject.find(z).text
-                #convert x,y,z to a position vector
-                positionVector = []
-                positionVector.append(x)
-                positionVector.append(y)
-                positionVector.append(z)
-                cpList.append(CriticalPoint(rank,signature,positionVector))
+        if topologicalObject.tag == 'CP':
+            #add a CP to the scene with the appropriate data
+            rank = topologicalObject.find('rank').text
+            signature = topologicalObject.find('signature').text
+            x = topologicalObject.find('x').text
+            y = topologicalObject.find('y').text
+            z = topologicalObject.find('z').text
+            #convert x,y,z to a position vector
+            positionVector = []
+            positionVector.append(x)
+            positionVector.append(y)
+            positionVector.append(z)
+            cp = CriticalPoint(rank,signature,positionVector)
+            cp.printOut()
+            cpList.append(cp)
 
 class main():
 
