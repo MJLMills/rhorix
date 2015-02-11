7a1a157085b38d17eef99ebad04c3e4f76c45ee4
MJLMills <mjohnmills@gmail.com>
2015-02-10 20:38:48 -0800

    fixed errors preventing running in blender

--- QCT4Blender.py
+++ QCT4Blender.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import bpy
+import mathutils
 
 cpList = [] #list of CrticalPoint objects
 ailList = [] #list of AtomicInteractionLine objects
@@ -17,19 +18,21 @@ class CriticalPoint():
     def printOut(self): # just for debugging
         print('RANK:      ' + self.rank)
         print('SIGNATURE: ' + self.signature)
-        print('POSITION:  ' + self.position[0] + ' ' + self.position[1] + ' ' + self.position[2])
+        print('POSITION:  ' + self.position.x + ' ' + self.position.y + ' ' + self.position.z)
 
 #*#*#*#*#*#*#*#*#*#*# CLASS DEFINITION
 
 class AtomicInteractionLine():
 
     def __init__(self):
+        print()
 
 #*#*#*#*#*#*#*#*#*#*# CLASS DEFINITION
 
 class Surface():
 
     def __init__(self):
+        print()
 
 #*#*#*#*#*#*#*#*#*#*# CLASS DEFINITION
 
@@ -71,31 +74,29 @@ class QCTBlender(bpy.types.Operator):
          mat.specular_intensity = 0.5
          mat.alpha = 1
          mat.ambient = 1
-    def readTopology(filepath):
-        #given an open topology file create all the corresponding python objects
-        topologyTree = ET.parse(filepath)
-        topologyRoot = topologyTree.getroot()
-        if topologyRoot.tag != 'topology':
-            print('Not a Topology File')
-            exit(1)
 
-        for topologicalObject in topologyRoot:
+     def readTopology(filepath):
+         #given an open topology file create all the corresponding python objects
+         topologyTree = ET.parse(filepath)
+         topologyRoot = topologyTree.getroot()
+         if topologyRoot.tag != 'topology':
+             print('Not a Topology File')
+             exit(1)
 
-            if topologicalObject.tag == 'CP':
-                #add a CP to the scene with the appropriate data
-                rank = topologicalObject.find('rank').text
-                signature = topologicalObject.find('signature').text
-                x = topologicalObject.find('x').text
-                y = topologicalObject.find('y').text
-                z = topologicalObject.find('z').text
-                #convert x,y,z to a position vector - has to be a less verbose way?
-                positionVector = []
-                positionVector.append(x)
-                positionVector.append(y)
-                positionVector.append(z)
-                cp = CriticalPoint(rank,signature,positionVector)
-                cp.printOut() # for debugging - no need to keep the cp reference if not calling this
-                cpList.append(cp)
+         for topologicalObject in topologyRoot:
+
+             if topologicalObject.tag == 'CP':
+                 #add a CP to the scene with the appropriate data
+                 rank = topologicalObject.find('rank').text
+                 signature = topologicalObject.find('signature').text
+                 x = topologicalObject.find('x').text
+                 y = topologicalObject.find('y').text
+                 z = topologicalObject.find('z').text
+                 #convert x,y,z to a position vector - has to be a less verbose way?
+                 positionVector = mathutils.Vector((x,y,z))
+                 cp = CriticalPoint(rank,signature,positionVector)
+                 cp.printOut() # for debugging - no need to keep the cp reference if not calling this
+                 cpList.append(cp)
 
 #*#*#*#*#*#*#*#*#*#* SCRIPT FUNCTION DEFINITIONS
 
