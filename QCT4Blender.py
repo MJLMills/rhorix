1ea3ae48f9941630678cd05c78a1097cab29c63b
MJLMills <mjohnmills@gmail.com>
2015-02-10 21:03:05 -0800

    fixed problems - CPs can now be read and created in top

--- QCT4Blender.py
+++ QCT4Blender.py
@@ -18,7 +18,7 @@ class CriticalPoint():
     def printOut(self): # just for debugging
         print('RANK:      ' + self.rank)
         print('SIGNATURE: ' + self.signature)
-        print('POSITION:  ' + self.position.x + ' ' + self.position.y + ' ' + self.position.z)
+        print('POSITION:  ' + str(self.position.x) + ' ' + str(self.position.y) + ' ' + str(self.position.z))
 
 #*#*#*#*#*#*#*#*#*#*# CLASS DEFINITION
 
@@ -38,65 +38,35 @@ class Surface():
 
 class QCTBlender(bpy.types.Operator):
 
-     bl_idname = "qct.import_topology"
-     bl_label = "Import Topology"
+    bl_idname = "qct.import_topology"
+    bl_label = "Import Topology"
  
-     filepath = bpy.props.StringProperty(subtype="FILE_PATH")
+    filepath = bpy.props.StringProperty(subtype="FILE_PATH")
  
-     def execute(self, context):
-          print("QCT4Blender Executed")
-          print("Opening File: " + self.filepath)
-          readTopology(self.filepath)
-          createBlenderObjects()
-          return{'FINISHED'}
+    def execute(self, context):
+        print("QCT4Blender Executed")
+        print("Opening File: " + self.filepath)
+        readTopology(self.filepath)
+        createBlenderObjects()
+        return{'FINISHED'}
   
-     def invoke(self, context, event):
-          print("QCT4Blender Invoked")
-          context.window_manager.fileselect_add(self)
-          return {'RUNNING_MODAL'}
+    def invoke(self, context, event):
+        print("QCT4Blender Invoked")
+        context.window_manager.fileselect_add(self)
+        return {'RUNNING_MODAL'}
 
 
-     def createBlenderObjects():
-         for cp in cpList:
-             #the sphere should be created with the location of the cp object
-             position = (0,0,0)
-             cpSphere = bpy.ops.mesh.primitive_uv_sphere_add(location=position)
-
-
-     def createAtomMaterial(name):
-         #name is the element of the atom
-         mat = bpy.data.materials.new(name)
-         mat.diffuse_color = (1,0,0) #look up the color from the element
-         mat.diffuse_shader = 'LAMBERT'
-         mat.diffuse_intensity = 1.0
-         mat.specular_color = (1,1,1)
-         mat.specular_shader = 'COOKTORR'
-         mat.specular_intensity = 0.5
-         mat.alpha = 1
-         mat.ambient = 1
-
-     def readTopology(filepath):
-         #given an open topology file create all the corresponding python objects
-         topologyTree = ET.parse(filepath)
-         topologyRoot = topologyTree.getroot()
-         if topologyRoot.tag != 'topology':
-             print('Not a Topology File')
-             exit(1)
-
-         for topologicalObject in topologyRoot:
-
-             if topologicalObject.tag == 'CP':
-                 #add a CP to the scene with the appropriate data
-                 rank = topologicalObject.find('rank').text
-                 signature = topologicalObject.find('signature').text
-                 x = topologicalObject.find('x').text
-                 y = topologicalObject.find('y').text
-                 z = topologicalObject.find('z').text
-                 #convert x,y,z to a position vector - has to be a less verbose way?
-                 positionVector = mathutils.Vector((x,y,z))
-                 cp = CriticalPoint(rank,signature,positionVector)
-                 cp.printOut() # for debugging - no need to keep the cp reference if not calling this
-                 cpList.append(cp)
+    def createAtomMaterial(name):
+        #name is the element of the atom
+        mat = bpy.data.materials.new(name)
+        mat.diffuse_color = (1,0,0) #look up the color from the element
+        mat.diffuse_shader = 'LAMBERT'
+        mat.diffuse_intensity = 1.0
+        mat.specular_color = (1,1,1)
+        mat.specular_shader = 'COOKTORR'
+        mat.specular_intensity = 0.5
+        mat.alpha = 1
+        mat.ambient = 1
 
 #*#*#*#*#*#*#*#*#*#* SCRIPT FUNCTION DEFINITIONS
 
@@ -107,6 +77,34 @@ def register():
 def unregister():
     print("Deregistering Classes")
     bpy.utils.unregister_class(QCTBlender)
+
+def readTopology(filepath):
+    #given an open topology file create all the corresponding python objects
+    topologyTree = ET.parse(filepath)
+    topologyRoot = topologyTree.getroot()
+    if topologyRoot.tag != 'topology':
+        print('Not a Topology File')
+        exit(1)
+
+    for topologicalObject in topologyRoot:
+
+        if topologicalObject.tag == 'CP':
+            #add a CP to the scene with the appropriate data
+            rank = topologicalObject.find('rank').text
+            signature = topologicalObject.find('signature').text
+            x = topologicalObject.find('x').text
+            y = topologicalObject.find('y').text
+            z = topologicalObject.find('z').text
+            #convert x,y,z to a position vector - has to be a less verbose way?
+            positionVector = mathutils.Vector((float(x),float(y),float(z)))
+            cp = CriticalPoint(rank,signature,positionVector)
+            cp.printOut() # for debugging - no need to keep the cp reference if not calling this
+            cpList.append(cp)
+
+def createBlenderObjects():
+    for cp in cpList:
+        #the sphere should be created with the location of the cp object
+        cpSphere = bpy.ops.mesh.primitive_uv_sphere_add(location=cp.position)
 
 if __name__ == "main":
  register()
