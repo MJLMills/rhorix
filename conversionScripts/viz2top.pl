7e10be064cd45f510ef264bfa34775a5088e8364
MJLMills <mjohnmills@gmail.com>
2015-02-12 21:40:57 -0800

    added more read ifs to conversion script

--- conversionScripts/viz2top.pl
+++ conversionScripts/viz2top.pl
@@ -6,73 +6,113 @@
 $nFiles = @vizFiles;
 print "$nFiles VIZ FILES IN FOLDER\n";
 
-for ($file=0;$file<@vizFiles;$file++) {
-
-open(VIZ,"<","$vizFiles[$file]") || die "SPECIFIED FILE $vizFiles[$file] COULD NOT BE OPENED\n";
-@vizContents = <VIZ>;
-close VIZ;
-chomp(@vizContents);
-
 open(TOP,">","h2o\.top");
 print TOP "\<topology\>\n";
 
-for ($line=0; $line<@vizContents; $line++) {
-  if ($vizContents[$line] =~ m/CP\#\s+(\d+)\s+Coords\s+\=\s+(-?\d+\.\d+)E([+-]\d+)\s+(-?\d+\.\d+)E([+-]\d+)\s+(-?\d+\.\d+)E([+-]\d+)/) {
+for ($file=0;$file<@vizFiles;$file++) {
+
+ print "READING $vizFiles[$file]\n";
+  open(VIZ,"<","$vizFiles[$file]") || die "SPECIFIED FILE $vizFiles[$file] COULD NOT BE OPENED\n";
+  @vizContents = <VIZ>;
+  close VIZ;
+  chomp(@vizContents);
+
+  for ($line=0; $line<@vizContents; $line++) {
+    if ($vizContents[$line] =~ m/CP\#\s+(\d+)\s+Coords\s+\=\s+(-?\d+\.\d+)E([+-]\d+)\s+(-?\d+\.\d+)E([+-]\d+)\s+(-?\d+\.\d+)E([+-]\d+)/) {
         
-    $x = $2 * (10 ** $3);
-    $y = $4 * (10 ** $5);
-    $z = $6 * (10 ** $7);
+      $x = $2 * (10 ** $3);
+      $y = $4 * (10 ** $5);
+      $z = $6 * (10 ** $7);
         
-    if ($vizContents[$line+1] =~ m/Type\s+\=\s+\((-?\d+)\,(-?\d+)\)/) {
-      $rank = $1; $signature = $2;
-    }
+      if ($vizContents[$line+1] =~ m/Type\s+\=\s+\((-?\d+)\,(-?\d+)\)/) {
+        $rank = $1; $signature = $2;
+      }
 
 #    print "FOUND CP: rank=$rank; signature=$signature\n";
-    printCP();
+#      printCP();
 
-  } elsif ($vizContents[$line] =~ m/(\d+)\s+sample points along/) {
+    } elsif ($vizContents[$line] =~ m/(\d+)\s+sample points along/) {
 
-    $nPoints = $1;
-    my @ailPoints_x; my @ailPoints_y; my @ailPoints_z;
-#    print "FOUND LINE\: $nPoints POINTS\n";
-    for ($ailLine=$line+1;$ailLine<$line+$nPoints+1;$ailLine++) {
-      if ($vizContents[$ailLine] =~ m/\s+(-?\d+\.\d+)E([-+]\d+)\s+(-?\d+\.\d+)E([-+]\d+)\s+(-?\d+\.\d+)E([-+]\d+)\s+(-?\d+\.\d+)E([-+]\d+)\s+/) {
-        $x = $1 * (10 ** $2); push(@ailPoints_x, $x);
-        $y = $3 * (10 ** $4); push(@ailPoints_y, $y);
-        $z = $5 * (10 ** $6); push(@ailPoints_z, $z);
-      } else {
-        die "Malformed AIL point\n";
-      }
-    }
-    printLine(\@ailPoints_x,\@ailPoints_y,\@ailPoints_z);
-
-  } elsif ($vizContents[$line] =~ m/\<IAS\s+Path\>/) {
-    
-    if ($vizContents[$line+1] =~ m/\s+\d+\s+(\d+)/) {
       $nPoints = $1;
-#      print "Found IAS Path with $nPoints points\n";
-
       my @ailPoints_x; my @ailPoints_y; my @ailPoints_z;
-      for ($iasLine=$line+2;$iasLine<$line+$nPoints+1;$iasLine++) {
-        if ($vizContents[$iasLine] =~ m/\s+(-?\d+\.\d+)E([-+]\d+)\s+(-?\d+\.\d+)E([-+]\d+)\s+(-?\d+\.\d+)E([-+]\d+)\s+(-?\d+\.\d+)E([-+]\d+)/) {
+#    print "FOUND LINE\: $nPoints POINTS\n";
+      for ($ailLine=$line+1;$ailLine<$line+$nPoints+1;$ailLine++) {
+        if ($vizContents[$ailLine] =~ m/\s+(-?\d+\.\d+)E([-+]\d+)\s+(-?\d+\.\d+)E([-+]\d+)\s+(-?\d+\.\d+)E([-+]\d+)\s+(-?\d+\.\d+)E([-+]\d+)\s+/) {
           $x = $1 * (10 ** $2); push(@ailPoints_x, $x);
           $y = $3 * (10 ** $4); push(@ailPoints_y, $y);
           $z = $5 * (10 ** $6); push(@ailPoints_z, $z);
         } else {
-          die "Malformed IAS Path point\:\t$vizContents[$iasLine]\n";
+          die "Malformed AIL point\n";
         }
       }
-      printLine(\@ailPoints_x,\@ailPoints_y,\@ailPoints_z);
+#      printLine(\@ailPoints_x,\@ailPoints_y,\@ailPoints_z);
 
-    } else {
-      die "Malformed line in .iasviz file\n";
+    } elsif ($vizContents[$line] =~ m/\<IAS\s+Path\>/) {
+    
+      if ($vizContents[$line+1] =~ m/\s+\d+\s+(\d+)/) {
+
+        $nPoints = $1;
+
+        my @ailPoints_x; my @ailPoints_y; my @ailPoints_z;
+        for ($iasLine=$line+2;$iasLine<$line+$nPoints+1;$iasLine++) {
+          if ($vizContents[$iasLine] =~ m/\s+(-?\d+\.\d+)E([-+]\d+)\s+(-?\d+\.\d+)E([-+]\d+)\s+(-?\d+\.\d+)E([-+]\d+)\s+(-?\d+\.\d+)E([-+]\d+)/) {
+            $x = $1 * (10 ** $2); push(@ailPoints_x, $x);
+            $y = $3 * (10 ** $4); push(@ailPoints_y, $y);
+            $z = $5 * (10 ** $6); push(@ailPoints_z, $z);
+          } else {
+            die "Malformed IAS Path point\:\t$vizContents[$iasLine]\n";
+          }
+        }
+#        printLine(\@ailPoints_x,\@ailPoints_y,\@ailPoints_z);
+
+      } else {
+        die "Malformed line in .iasviz file\n";
+      }
+
+    } elsif ($vizContents[$line] =~ m/\<Intersections of Integration Rays With IsoDensity Surfaces\>/) {
+
+      if ($vizContents[$line+1] =~ m/\s+\d+\s+-?\d+\.\d+E[-+]\d+\s+-?\d+\.\d+E[-+]\d+\s+-?\d+\.\d+E[-+]\d+\s+(\d+)/) {
+        $nPoints = $1;
+        print "FOUND RAY/SURF INTERSECTIONS - $nPoints POINTS\n";
+        my @ailPoints_x; my @ailPoints_y; my @ailPoints_z;
+        for ($iasLine=$line+2;$iasLine<$line+$nPoints+1;$iasLine++) {
+          if ($vizContents[$iasLine] =~ m/\s+(-?\d+\.\d+)E([-+]\d+)\s+(-?\d+\.\d+)E([-+]\d+)\s+(-?\d+\.\d+)E([-+]\d+)/) {
+            $x = $1 * (10 ** $2); push(@ailPoints_x, $x);
+            $y = $3 * (10 ** $4); push(@ailPoints_y, $y);
+            $z = $5 * (10 ** $6); push(@ailPoints_z, $z);
+          } else {
+            die "Malformed IAS Path point\:\t$vizContents[$iasLine]\n";
+          }
+        }
+        printLine(\@ailPoints_x,\@ailPoints_y,\@ailPoints_z);
+
+      } else {
+        die "Malformed IAS Path point\:\t$vizContents[$iasLine]\n";
+      }
+
+    } elsif ($vizContents[$line] =~ m/\<Intersections of Integration Rays with Atomic Surface\>/) {
+
+      if ($vizContents[$line+1] =~ m/\s+\d+\s+(\d+)\s+\d+/) {
+        $nPoints = $1;
+#        print "FOUND RAY/SURF INTERSECTIONS - $nPoints POINTS\n";
+        my @ailPoints_x; my @ailPoints_y; my @ailPoints_z;
+        for ($iasLine=$line+2;$iasLine<$line+$nPoints+1;$iasLine++) {
+          if ($vizContents[$iasLine] =~ m/\s+\d+\s+\d+\s+\d+\s+(-?\d+\.\d+)E([-+]\d+)\s+(-?\d+\.\d+)E([-+]\d+)\s+(-?\d+\.\d+)E([-+]\d+)\s+(-?\d+\.\d+)E([-+]\d+)/) {
+            $x = $1 * (10 ** $2); push(@ailPoints_x, $x);
+            $y = $3 * (10 ** $4); push(@ailPoints_y, $y);
+            $z = $5 * (10 ** $6); push(@ailPoints_z, $z);
+          } else {
+            die "Malformed IAS Path point\:\t$vizContents[$iasLine]\n";
+          }
+        }
+#        printLine(\@ailPoints_x,\@ailPoints_y,\@ailPoints_z);
+      } else {
+        die "Malformed line in .iasviz file\n";
+      }      
     }
-
   }
-
-}
-
 } # end over files
+
 print TOP "\<\/topology\>\n";
 close TOP;
 
@@ -81,6 +121,7 @@ sub printLine {
   #sub must receive three lists as references (i.e. \@array1, \@array2, \@array3)
   my ($xPoints, $yPoints, $zPoints) = @_;
   $nPoints = scalar(@$xPoints);
+  print "writing $nPoints points\n";
 
   print TOP "  \<LINE\>\n";
   for ($point=0;$point<$nPoints;$point++) {
