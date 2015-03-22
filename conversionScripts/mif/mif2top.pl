ec28bfd6dfbd99f6f1f5fa9224658f58eb3d9080
MJLMills <mjohnmills@gmail.com>
2015-03-22 15:47:17 -0700

    changed script to read/write connectivity of surfaces

--- conversionScripts/mif/mif2top.pl
+++ conversionScripts/mif/mif2top.pl
@@ -1,4 +1,4 @@
-open(MIF,"<","compound1\.mif");
+open(MIF,"<","ALANINE0000\.mif");
 @mifContents = <MIF>;
 close MIF;
 chomp(@mifContents);
@@ -51,55 +51,67 @@ for ($line=0;$line<@mifContents;$line++) {
         last;
       }
     }
-  } elsif ($mifContents[$line] =~ m/atom\s+(\w+)\_(\d+)/) {
+  } elsif ($mifContents[$line] =~ m/atom\s+(\w+)\_(\d+)/ || $mifContents[$line] =~ m/surf\s+(\w+)\_(\d+)/) {
 
     $atom = "$1$2";
+    $pointID = 0;
     #write the surface out - mifs are not in a consistent format so all possibilities are needed
     #ORDER IS SUPER IMPORTANT!!!
+    my @edgeA; my @edgeB;
     my @ailCoords_x; my @ailCoords_y; my @ailCoords_z;
+
     for ($surfLine=$line+2;$surfLine<@mifContents;$surfLine++) {
       if ($mifContents[$surfLine] =~ m/(-?\d+\.\d+)E([+-]\d+)\s+(-?\d+\.\d+)E([+-]\d+)\s+(-?\d+\.\d+)E([+-]\d+)/) {
         # 1 1 1
         $x = $1 * (10 ** $2); $y = $3 * (10 ** $4); $z = $5 * (10 ** $6); 
         push(@ailCoords_x,$x); push(@ailCoords_y,$y); push(@ailCoords_z,$z);
+        push(@edgeA,$pointID); $pointID++; push(@edgeB,$pointID);
 
       } elsif ($mifContents[$surfLine] =~ m/(-?\d+\.\d+)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)E([+-]\d+)/) {
         # 0 0 1
         $x = $1; $y = $2; $z = $3 * (10 ** $4);
         push(@ailCoords_x,$x); push(@ailCoords_y,$y); push(@ailCoords_z,$z);
+        push(@edgeA,$pointID); $pointID++; push(@edgeB,$pointID);
 
       } elsif ($mifContents[$surfLine] =~ m/(-?\d+\.\d+)\s+(-?\d+\.\d+)E([+-]\d+)\s+(-?\d+\.\d+)E([+-]\d+)/) {
         # 0 1 1
         $x = $1; $y = $2 * (10 ** $3); $z = $4 * (10 ** $5);
         push(@ailCoords_x,$x); push(@ailCoords_y,$y); push(@ailCoords_z,$z);
+        push(@edgeA,$pointID); $pointID++; push(@edgeB,$pointID);
 
       } elsif ($mifContents[$surfLine] =~ m/(-?\d+\.\d+)E([+-]\d+)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)E([+-]\d+)/) {
         # 1 0 1
         $x = $1 * (10 ** $2); $y = $3; $z = $4 * (10 ** $5); 
         push(@ailCoords_x,$x); push(@ailCoords_y,$y); push(@ailCoords_z,$z);
+        push(@edgeA,$pointID); $pointID++; push(@edgeB,$pointID);
 
       } elsif ($mifContents[$surfLine] =~ m/(-?\d+\.\d+)E([+-]\d+)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)/) {
         # 1 0 0
         $x = $1 * (10 ** $2); $y = $3; $z = $4;
         push(@ailCoords_x,$x); push(@ailCoords_y,$y); push(@ailCoords_z,$z);
+        push(@edgeA,$pointID); $pointID++; push(@edgeB,$pointID);
 
       } elsif ($mifContents[$surfLine] =~ m/(-?\d+\.\d+)E([+-]\d+)\s+(-?\d+\.\d+)E([+-]\d+)\s+(-?\d+\.\d+)/) {
         # 1 1 0
         $x = $1 * (10 ** $2); $y = $3 * (10 ** $4); $z = $5;
         push(@ailCoords_x,$x); push(@ailCoords_y,$y); push(@ailCoords_z,$z);
+        push(@edgeA,$pointID); $pointID++; push(@edgeB,$pointID);
 
       } elsif ($mifContents[$surfLine] =~ m/(-?\d+\.\d+)\s+(-?\d+\.\d+)E([+-]\d+)\s+(-?\d+\.\d+)/) {
         # 0 1 0
         $x = $1; $y = $2 * (10 ** $3); $z = $4;
         push(@ailCoords_x,$x); push(@ailCoords_y,$y); push(@ailCoords_z,$z);
+        push(@edgeA,$pointID); $pointID++; push(@edgeB,$pointID);
 
       } elsif ($mifContents[$surfLine] =~ m/(-?\d+\.\d+)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)/) {
         # 0 0 0
         $x = $1; $y = $2; $z = $3;
         push(@ailCoords_x,$x); push(@ailCoords_y,$y); push(@ailCoords_z,$z);
+        push(@edgeA,$pointID); $pointID++; push(@edgeB,$pointID);
 
       } else {
         $line = $surfLine - 1;
+        pop(@edgeA); pop(@edgeB); #strip the erroneous point from the end of the graph arrays
         last;
       }
     }
@@ -108,12 +120,97 @@ for ($line=0;$line<@mifContents;$line++) {
       $ailCoords_y[$point] *= 10;
       @ailCoords_z[$point] *= 10;
     }
-    printSurf(\@ailCoords_x, \@ailCoords_y, \@ailCoords_z);
+#    checkDuplicates(\@ailCoords_x);
+    printSurf(\@ailCoords_x, \@ailCoords_y, \@ailCoords_z, \@edgeA, \@edgeB);
   }
 }
 
 print TOP "\<\/topology\>\n";
 close TOP;
+
+#this is a tool for debugging - not really needed
+sub checkDuplicates {
+
+  my ($array) = @_; $nPoints = @$array;
+  print "CHECKING DUPLICATES FOR $array POINTS\n";
+  for ($i=0;$i<@$array;$i++) {
+
+    $store = @$array[$i];
+
+    for ($j=0;$j<@$array;$j++) {
+      if (@$array[$j] == $store && $i != $j) {
+        print "DUPLICATE FOUND\n";
+      }
+    }
+  }
+
+}
+
+sub reformatSurface {
+
+  my ($xPoints,$yPoints,$zPoints) = @_;
+  my @isRedundant; my @xNew, @yNew, @zNew;
+
+  for ($i=0;$i<@xPoints;$i++) {
+    $isRedundant[$i] = 0;
+  }
+
+  $n_x = @xPoints; $n_y = @yPoints; $n_z = @zPoints;
+  if ($n_x == $n_y && $n_y == $n_z) {
+
+    for ($point=0;$point<@xPoints;$point++) {
+
+      if ($isRedundant[$point] == 1) {
+        next;
+      } else {
+
+        push(@xNew,$xPoints[$point]); push(@yNew,$yPoints[$point]); push(@zNew,$zPoints[$point]);
+
+        $k = @xNew - 1;
+
+        for ($j=$point+1;$j<@xPoints;$j++) {
+
+          if ($xPoints[$point] == $xPoints[$j] && $yPoints[$point] == $yPoints[$j] && $zPoints[$point] == $zPoints[$j]) {
+            $isRedundant[$j] = 1; 
+            for ($m=0;$m<@edges;$m++) {
+              if ($edges[0][$m] == j) {
+                $edges[0][$m] = k;
+              } elsif ($edges[1][$m] == j) {
+                $edges[1][$m] = k;
+              }
+            }
+          }
+
+        }
+
+      }
+
+    }
+  } else {
+    die "ERROR: MISMATCHED COORDINATE ARRAYS IN reformatSurface SUBROUTINE\n";
+  }
+
+}
+
+sub printGraph {
+
+  my ($edgeA, $edgeB) = @_;
+
+  $n_A = @$edgeA; $n_B = @$edgeB;
+  if ($n_A == $n_B) {
+
+    for ($edge=0;$edge<@$edgeA;$edge++) {
+      print TOP "    \<edge\>";
+      print TOP " \<A\>@$edgeA[$edge]\<\/A\>";
+      print TOP " \<B\>@$edgeB[$edge]\<\/B\>";
+      print TOP " \<\/edge\>\n";
+    }
+
+  } else {
+    print "N_A = $n_A\nN_B = $n_B\n";
+    die "ERROR: MISMATCHED GRAPH ARRAYS IN printGraph SUBROUTINE\n";
+  }
+}
 
 sub printLine {
 
@@ -140,7 +237,7 @@ sub printLine {
 sub printSurf {
 
   #sub must receive three lists as references (i.e. \@array1, \@array2, \@array3)
-  my ($xPoints, $yPoints, $zPoints) = @_;
+  my ($xPoints, $yPoints, $zPoints, $edgeA, $edgeB) = @_;
   $nPoints = scalar(@$xPoints);
 #  print "writing $nPoints points\n";
 
@@ -153,6 +250,9 @@ sub printSurf {
     printf TOP " \<z\>%8.5f\<\/z\>", @$zPoints[$point];
     print TOP " \<\/vector\>\n";
   }
+
+  printGraph(\@$edgeA,\@$edgeB);
+
   print TOP "  \<\/SURFACE\>\n";
 
 }
