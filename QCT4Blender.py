042b56ec6d010d553dd756f9e7c2566a20c04ec1
MJLMills <mjohnmills@gmail.com>
2016-01-18 08:41:27 -0800

    Added Import Topology button to UI panel

--- QCT4Blender.py
+++ QCT4Blender.py
@@ -93,6 +93,7 @@ class QCTPanel(bpy.types.Panel):
 
     def draw(self,context):
         uiColumn = self.layout.column(align=True)
+        uiColumn.operator("qct.import_topology", text="Import Topology")
         uiColumn.operator("qct.select_nuclei", text="Select Nuclei")
 
 #*#*#*#*#*#*#*#*#*#* SCRIPT FUNCTION DEFINITIONS
