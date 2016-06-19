f4c62c1a873ae3886bf04c9487ce91e8b97b62d3
MJLMills <mjohnmills@gmail.com>
2016-06-18 18:53:27 -0700

    added mif collation script

---
+++ conversionScripts/collateMifs.pl
@@ -0,0 +1,102 @@
+#!/usr/bin/perl -w
+# Matthew J L Mills
+
+@mifList = &listFilesOfType("mif");
+
+my @outputAils;
+my @outputSurfaces;
+$totalTime = 0;
+
+foreach(@mifList) {
+#  print " PARSING $_\n";
+  if ($_ =~ m/(.*)\-AIL/) {
+    $fileName = "$1";
+    @outputAils = &readFile($_); 
+  } else {
+    if ($_ =~ m/(\w+\d+)\.mif/) {
+      $atomName = $1;
+
+      @moutContents = &readFile("$atomName\.mout");
+      $time = &parseTime(\@moutContents);
+      if ($time != 0) {
+
+        $totalTime += $time;
+        my @mifContents = &readFile($_);
+        foreach (@mifContents) {
+          if ($_ =~ m/atom\s+\d+/) {
+            push(@outputSurfaces,"atom $atomName");
+          } else {
+            push(@outputSurfaces,$_);
+           }
+        }
+
+      } else {
+        print "$_ Not Complete\n";
+      }
+    } else {
+      print "Ignoring File\: $_\n";
+    }  
+  }
+}
+
+&printOutput(\@outputAils,\@outputSurfaces,$fileName);
+print "Total Triangulation Time\: $totalTime s\n";
+
+sub parseTime {
+
+  foreach(@{$_[0]}) {
+    if ($_ =~ m/INTEGRATION\s+TIME\s+(-?\d+\.\d+)/) {
+      return $1;
+    }
+  }
+  return 0;
+}
+
+sub printOutput {
+
+  open(MIF,">","$fileName\.mif") || die "Cannot create output file\: $fileName\.mif\n";
+
+  foreach(@{$_[0]}) {
+    print MIF "$_\n";
+  }
+
+  foreach(@{$_[1]}) {
+    print MIF "$_\n";
+  }
+
+  close MIF;
+
+}
+
+
+sub readFile {
+
+  open(INP,"<","$_[0]") || die "Cannot open input file\: $_[0] for reading\n";
+  my @inpContents = <INP>;
+  chomp(@inpContents);
+  close INP;
+  return @inpContents;
+
+}
+
+sub listFilesOfType {
+
+  my $ext = $_[0];
+  my @fileList;
+
+  my $dir = ".";
+  opendir my($dirHandle), $dir || die "Cannot open directory $dir\: $!";
+  for (readdir $dirHandle) {
+    if (-d $_) { next; }
+    if ($_ =~ m/^[.]/) { next; }
+    if ($_ =~ m/(.+)[.]$ext/) {
+      push(@fileList,"$_");
+    }
+  }
+
+  closedir $dirHandle;
+  @fileList = sort(@fileList);
+  return @fileList;
+
+}
+
