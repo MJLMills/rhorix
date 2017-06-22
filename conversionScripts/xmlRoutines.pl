b05914e2578d8a6265cdb4507439a93a8f0990fc
MJLMills <mjohnmills@gmail.com>
2017-06-22 14:39:02 -0700

    place XML routines in separate file

---
+++ conversionScripts/xmlRoutines.pl
@@ -0,0 +1,38 @@
+### XML File Output Subroutines ###
+
+# writePCData - Write a single parsed character data XML element on a single line
+# Arguments: $_[0] - Name of the element
+#            $_[1] - Value of the element
+sub writePCData {
+  print "\<$_[0]\>$_[1]\<\/$_[0]\>\n";
+}
+
+# openTag - Write an XML open tag for an element
+# Arguments: $_[0] - Name of the element
+sub openTag {
+  print "\<$_[0]\>\n";
+}
+
+# closeTag - Write an XML close tag for an element
+# Arguments: $_[0] - Name of the element
+sub closeTag {
+  print "\<\/$_[0]\>\n";
+}
+
+# writeXMLHeader - Write the header of an XML file
+# Arguments: $_[0] - XML version number
+#            $_[1] - Encoding name
+#            $_[2] - Name of root XML tag
+#            $_[3] - Path to DTD file
+sub writeXMLHeader {
+
+  $version  = $_[0];
+  $encoding = $_[1];
+  $root     = $_[2];
+  $dtdPath  = $_[3];
+
+  print "\<\?xml version\=\"$version\" encoding=\"$encoding\"\?\>\n";
+  print "\<\!DOCTYPE $root PUBLIC \"ID\" \"$dtdPath\"\>\n";
+
+}
+
