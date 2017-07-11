2cc99eaab76e901487e10893c96127d2ae0679f7
MJLMills <mjohnmills@gmail.com>
2017-07-10 18:16:20 -0700

    change tabs to 4-spaces PEP8

--- Mapping.py
+++ Mapping.py
@@ -10,22 +10,22 @@ import bpy
 
 def drawTopology(topology):
 
-	drawCriticalPoints(topology.critical_points)
-	drawGradientVectorField(topology.gradient_vector_field)
+    drawCriticalPoints(topology.critical_points)
+    drawGradientVectorField(topology.gradient_vector_field)
 
 def drawGradientVectorField(gradient_vector_field):
-	drawMolecularGraph(gradient_vector_field.molecular_graph)
-	drawAtomicBasins(gradient_vector_field.atomic_basins)
-	drawEnvelopes(gradient_vector_field.envelopes)
-	drawAtomicSurfaces(gradient_vector_field.atomic_surfaces)
-	drawRingSurfaces(gradient_vector_field.ring_surfaces)
-	drawRings(gradient_vector_field.rings)
-	drawCages(gradient_vector_field.cages)
+    drawMolecularGraph(gradient_vector_field.molecular_graph)
+    drawAtomicBasins(gradient_vector_field.atomic_basins)
+    drawEnvelopes(gradient_vector_field.envelopes)
+    drawAtomicSurfaces(gradient_vector_field.atomic_surfaces)
+    drawRingSurfaces(gradient_vector_field.ring_surfaces)
+    drawRings(gradient_vector_field.rings)
+    drawCages(gradient_vector_field.cages)
 
 def drawCriticalPoints(critical_points):
-	for cp in critical_points:
-		cpSphere = bpy.ops.mesh.primitive_uv_sphere_add(location=cp.position_vector,size=0.1*elementRadii[cp.type],segments=32,ringcount=16)
-		# now attach an appropriate material to the sphere
+    for cp in critical_points:
+        cpSphere = bpy.ops.mesh.primitive_uv_sphere_add(location=cp.position_vector,size=0.1*elementRadii[cp.type],segments=32,ringcount=16)
+        # now attach an appropriate material to the sphere
 
 def drawMolecularGraph(molecular_graph):
 
@@ -34,45 +34,45 @@ def drawMolecularGraph(molecular_graph):
     bpy.context.object.name = 'AIL-BevelCircle'
     bpy.ops.transform.resize(value=(0.25,0.25,0.25))
 
-	for ail in molecular_graph:
-		drawAtomicInteractionLine(ail,material,bpy.data.objects['AIL-BevelCircle'])
+    for ail in molecular_graph:
+        drawAtomicInteractionLine(ail,material,bpy.data.objects['AIL-BevelCircle'])
 
 def drawAtomicInteractionLine(atomic_interaction_line,bevel):
-	for gradient_path in atomic_interaction_line.gradient_paths:
-		drawGradientPath(gradient_path,material,bevel)
+    for gradient_path in atomic_interaction_line.gradient_paths:
+        drawGradientPath(gradient_path,material,bevel)
 
 def drawAtomicBasins(atomic_basins):
-	for atomic_basin in atomic_basins:
-		for gradient_path in atomic_basin.gradient_paths:
-			drawGradientPath(gradient_path,material,bevel)
+    for atomic_basin in atomic_basins:
+        for gradient_path in atomic_basin.gradient_paths:
+            drawGradientPath(gradient_path,material,bevel)
 
 def drawEnvelopes(envelopes):
-	for envelope in envelopes:
-		if (not envelope.triangulation):
-			#draw point cloud
-		else:
-			drawMesh(triangulation)
+    for envelope in envelopes:
+        if (not envelope.triangulation):
+            #draw point cloud
+        else:
+            drawMesh(triangulation)
 
 def drawAtomicSurfaces(atomic_surfaces):
-	for atomic_surface in atomic_surfaces:
-		for interatomic_surface in atomic_surface.interatomic_surfaces:
-			if (not interatomic_surface.triangulation):
-				# draw points
-			else:
-				drawMesh(triangulation)
+    for atomic_surface in atomic_surfaces:
+        for interatomic_surface in atomic_surface.interatomic_surfaces:
+            if (not interatomic_surface.triangulation):
+                # draw points
+            else:
+                drawMesh(triangulation)
 
 def drawRingSurfaces(ring_surfaces):
-	for ring_surface in ring_surfaces:
-		for gradient_path in ring_surface.gradient_paths:
-			drawGradientPath(gradient_path)
+    for ring_surface in ring_surfaces:
+        for gradient_path in ring_surface.gradient_paths:
+            drawGradientPath(gradient_path)
 
 def drawRing(ring):
-	for atomic_interaction_line in ring.atomic_interaction_lines:
-		drawAtomicInteractionLine(atomic_interaction_line)
+    for atomic_interaction_line in ring.atomic_interaction_lines:
+        drawAtomicInteractionLine(atomic_interaction_line)
 
 def drawCage(cage):
-	for ring in cage.rings:
-		drawRing(ring)
+    for ring in cage.rings:
+        drawRing(ring)
 
 def drawMesh(triangulation,material):
 
@@ -89,22 +89,22 @@ def drawMesh(triangulation,material):
 
 def drawGradientPath(gradient_path,material,bevel):
 
-	weight = 1
-	cList = gradient_path.points
-	curveData = bpy.data.curves.new(type='CURVE')
-	curveData.dimesions - '3D'
+    weight = 1
+    cList = gradient_path.points
+    curveData = bpy.data.curves.new(type='CURVE')
+    curveData.dimesions - '3D'
 
-	objectData = bpy.data.objects.new('ObjCurve',curveData)
-   	objectData.location = (0,0,0)
-   	objectData.data.materials.append(material)
-   	objectData.data.bevel_object = bevel
-   	bpy.context.scene.objects.link(objectData)
+    objectData = bpy.data.objects.new('ObjCurve',curveData)
+       objectData.location = (0,0,0)
+       objectData.data.materials.append(material)
+       objectData.data.bevel_object = bevel
+       bpy.context.scene.objects.link(objectData)
 
     polyLine = curveData.splines.new('POLY')
     polyLine.points.add(len(cList)-1)
     for num in range(len(cList)):
-		x,y,z = cList[num].position_vector
-		polyLine.points[num].co = (x,y,z,weight)
+        x,y,z = cList[num].position_vector
+        polyLine.points[num].co = (x,y,z,weight)
 
 
 # need a dict that assigns materials based on element
