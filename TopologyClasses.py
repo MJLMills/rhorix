686bcba7f62a1238d69d56ee773f4a2264158a14
MJLMills <mjohnmills@gmail.com>
2017-07-10 17:19:01 -0700

    replace tabs with spaces per PEP8

--- TopologyClasses.py
+++ TopologyClasses.py
@@ -10,156 +10,156 @@ import math
 
 class Topology():
 	
-	def __init__(self,nuclei,critical_points,gradient_vector_field):
-		self.nuclei                = nuclei
-		self.critical_points       = critical_points
-		self.gradient_vector_field = gradient_vector_field
+    def __init__(self,nuclei,critical_points,gradient_vector_field):
+        self.nuclei                = nuclei
+        self.critical_points       = critical_points
+        self.gradient_vector_field = gradient_vector_field
 
-	# Find the center of the distribution of critical points
-	def computeCenter():
+    # Find the center of the distribution of critical points
+    def computeCenter():
 
-		x_total = 0.0
-		y_total = 0.0
- 		z_total = 0.0
+        x_total = 0.0
+        y_total = 0.0
+        z_total = 0.0
 
-		for cp in critical_points:
-			x_total += cp.position[0]
-			y_total += cp.position[1]
-			z_total += cp.position[2]
+        for cp in critical_points:
+            x_total += cp.position[0]
+            y_total += cp.position[1]
+            z_total += cp.position[2]
 
-		N = len(critical_points)
-		x_origin = x_total / N
-		y_origin = y_total / N
-		z_origin = z_total / N
+        N = len(critical_points)
+        x_origin = x_total / N
+        y_origin = y_total / N
+        z_origin = z_total / N
 
-		return mathutils.Vector((float(x_origin),float(y_origin),float(z_origin)))
+        return mathutils.Vector((float(x_origin),float(y_origin),float(z_origin)))
 
-	# Get the radius of a sphere containing all the critical point objects
-	def computeRadius(center):
+    # Get the radius of a sphere containing all the critical point objects
+    def computeRadius(center):
 
-		max = float('-inf')
+        max = float('-inf')
 
-		for cp in critical_points:
-			position = cp.position - center
-			r = math.sqrt(position[0]*position[0] + position[1]*position[1] + position[2]*position[2])
+        for cp in critical_points:
+            position = cp.position - center
+            r = math.sqrt(position[0]*position[0] + position[1]*position[1] + position[2]*position[2])
 
-			if position[0] > max:
-				max = position[0]
+            if position[0] > max:
+                max = position[0]
 
-		return max
+        return max
 
 class Nucleus():
-	def __init__(self,element,position_vector):
-		self.element         = element
-		self.position_vector = position_vector
+    def __init__(self,element,position_vector):
+        self.element         = element
+        self.position_vector = position_vector
 
 class CriticalPoint(Point):
-	def __init__(self,position_vector,scalar_properties,rank,signature):
-		Point.__init__(self,position_vector,scalar_properties)
-		self.rank      = rank
-		self.signature = signature
+    def __init__(self,position_vector,scalar_properties,rank,signature):
+        Point.__init__(self,position_vector,scalar_properties)
+        self.rank      = rank
+        self.signature = signature
 
 class GradientVectorField():
-	def __init__(self,molecular_graph,atomic_basins,envelopes,atomic_surfaces,ring_surfaces,rings,cages):
-		self.molecular_graph = molecular_graph
-		self.atomic_basins   = atomic_basins
-		self.envelopes       = envelopes
-		self.atomic_surfaces = atomic_surfaces
-		self.ring_surfaces   = ring_surfaces
-		self.rings           = rings
-		self.cages           = cages
+    def __init__(self,molecular_graph,atomic_basins,envelopes,atomic_surfaces,ring_surfaces,rings,cages):
+        self.molecular_graph = molecular_graph
+        self.atomic_basins   = atomic_basins
+        self.envelopes       = envelopes
+        self.atomic_surfaces = atomic_surfaces
+        self.ring_surfaces   = ring_surfaces
+        self.rings           = rings
+        self.cages           = cages
 
 class MolecularGraph():
-	def __init__(self,atomic_interaction_lines):
-		self.atomic_interaction_lines = atomic_interaction_lines
+    def __init__(self,atomic_interaction_lines):
+        self.atomic_interaction_lines = atomic_interaction_lines
 
 class AtomicInteractionLine():
-	def __init__(self,gradient_paths):
-		self.gradient_paths = gradient_paths
+    def __init__(self,gradient_paths):
+        self.gradient_paths = gradient_paths
 
 class AtomicBasin():
-	def __init__(self,gradient_paths,critical_point):
-		self.gradient_paths = gradient_paths
+    def __init__(self,gradient_paths,critical_point):
+        self.gradient_paths = gradient_paths
 
-	def getNuclearAttractorCriticalPoint():
-		for gradient_path in gradient_paths:
-			for cp in gradient_path.critical_points:
-				if (cp.rank == 3 and cp.signature == -3):
-					return cp
+    def getNuclearAttractorCriticalPoint():
+        for gradient_path in gradient_paths:
+            for cp in gradient_path.critical_points:
+                if (cp.rank == 3 and cp.signature == -3):
+                    return cp
 
 # An envelope is a set of points with an optional triangulation thereof.
 # As it does not have gradient paths it must have an explicit critical point member.
 class Envelope():
-	def __init__(self,isovalue,points,triangulation,nacp):
-		self.isovalue      = isovalue
-		self.points        = points
-		self.triangulation = triangulation
-		self.nacp          = nacp
+    def __init__(self,isovalue,points,triangulation,nacp):
+        self.isovalue      = isovalue
+        self.points        = points
+        self.triangulation = triangulation
+        self.nacp          = nacp
 
 # An atomic surface is a set of interatomic surfaces sharing a common BCP
 class AtomicSurface():
-	def __init__(self,interatomic_surfaces):
-		self.interatomic_surfaces = interatomic_surfaces
+    def __init__(self,interatomic_surfaces):
+        self.interatomic_surfaces = interatomic_surfaces
 
 # The gradient paths of an interatomic surface share a single BCP
 class InteratomicSurface():
-	def __init__(self,gradient_paths,triangulation):
-		self.gradient_paths = gradient_paths
-		self.triangulation  = triangulation
+    def __init__(self,gradient_paths,triangulation):
+        self.gradient_paths = gradient_paths
+        self.triangulation  = triangulation
 
-	def getBondCriticalPoint():
-		for gradient_path in gradient_paths:
-			for cp in gradient_path.critical_points:
-				if (cp.rank == 3 and cp.signature == -1):
-					return cp
+    def getBondCriticalPoint():
+        for gradient_path in gradient_paths:
+            for cp in gradient_path.critical_points:
+                if (cp.rank == 3 and cp.signature == -1):
+                    return cp
 
 # A ring surface is a set of gradient paths sharing a single RCP
 # There are a set of ring paths - GPs from the RCP to BCPs
 # and the remainder connect the RCP to nuclei connected by the BCPs
 class RingSurface():
-	def __init__(self,gradient_paths):
-		self.gradient_paths = gradient_paths
+    def __init__(self,gradient_paths):
+        self.gradient_paths = gradient_paths
 
-	def getRingCriticalPoint():
-		for gradient_path in gradient_paths:
-			for cp in gradient_path.critical_points:
-				if (cp.rank == 3 and cp.signature == +1):
-					return cp
+    def getRingCriticalPoint():
+        for gradient_path in gradient_paths:
+            for cp in gradient_path.critical_points:
+                if (cp.rank == 3 and cp.signature == +1):
+                    return cp
 
 # A ring is the set of AILs bounding a RCP
 class Ring():
-	def __init__(self,atomic_interaction_lines):
-		self.atomic_interaction_lines = atomic_interaction_lines
+    def __init__(self,atomic_interaction_lines):
+        self.atomic_interaction_lines = atomic_interaction_lines
 
 # A cage is the set of rings bounding a CCP
 class Cage():
-	def __init__(self,rings):
-		self.rings = rings
+    def __init__(self,rings):
+        self.rings = rings
 
 class Point():
-	def __init__(self,position_vector,scalar_properties):
-		self.position_vector   = position_vector
-		self.scalar_properties = scalar_properties
+    def __init__(self,position_vector,scalar_properties):
+        self.position_vector   = position_vector
+        self.scalar_properties = scalar_properties
 
 class Triangulation():
-	def __init__(self,points,edges,faces):
-		self.points = points
-		self.edges  = edges
-		self.faces  = faces
+    def __init__(self,points,edges,faces):
+        self.points = points
+        self.edges  = edges
+        self.faces  = faces
 
 class Edge():
-	def __init__(self,a,b):
-		self.a = a
-		self.b = b
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
 
 class Face():
-	def __init__(self,a,b,c):
-		self.a = a
-		self.b = b
-		self.c = c
+    def __init__(self,a,b,c):
+        self.a = a
+        self.b = b
+        self.c = c
 
 class GradientPath():
-	def __init__(self,critical_points,points):
-		self.critical_points = critical_points
-		self.points = points
+    def __init__(self,critical_points,points):
+        self.critical_points = critical_points
+        self.points = points
 		
