ca323845d9fe14658ae05b4499851d043a998788
MJLMills <mjohnmills@gmail.com>
2017-07-13 13:56:26 -0700

    add description

--- conversionScripts/validateXML.pl
+++ conversionScripts/validateXML.pl
@@ -1,8 +1,10 @@
+# Dr. Matthew J L Mills - Rhorix v1.0 - June 2017
+# Script to check validity of supplied XML file against its DTD
+
 use Utilities     qw(checkArgs getExt readFile stripExt);
 use XmlRoutines   qw(checkValidity);
 
 my $topFile = &checkArgs(\@ARGV,"top");
 print "Validating $topFile\n";
 $result = checkValidity($topFile);
-
 print "RESULT\: $result\n";
