a42ed2492e0ba702bd73e193c07afaf2f3ce7a1a
MJLMills <mjohnmills@gmail.com>
2017-07-25 15:46:52 -0700

    change basviz reader for 2D case

--- conversionScripts/ParseViz.pm
+++ conversionScripts/ParseViz.pm
@@ -424,7 +424,7 @@ sub parseBasinFromBasviz {
 
   foreach($line=0; $line<@basvizContents; $line++) {
     if ($basvizContents[$line] =~ m/\<Basin\s+Path\>/) {
-      if ($basvizContents[$line+1] =~ m/(\d+)\s+\d+\s+\d+\s+-?\d+\.\d+E[+-]\d+\s+-?\d+\.\d+E[+-]\d+\s+-?\d+\.\d+E[+-]\d+/) {
+      if ($basvizContents[$line+1] =~ m/(\d+)\s+\d+\s+-?\d+\.\d+E[+-]\d+/) {
         $nPoints = $1;
       } else {
         die "Malformed header of Basin Path\: $basvizContents[$line+1]\n\n";
