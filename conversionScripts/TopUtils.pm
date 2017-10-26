7e376f8170e09abd76c419783b8e5b20c404a244
MJLMills <mjohnmills@gmail.com>
2017-10-26 16:43:30 -0700

    add module for general topology-related subs

---
+++ conversionScripts/TopUtils.pm
@@ -0,0 +1,17 @@
+# Utilities Perl Module
+# Dr. Matthew J L Mills - Rhorix v1.0 - June 2017
+# Subroutines for repeated tasks related to files and command-line arguments.
+
+package TopUtils;
+require Exporter;
+
+### Module Settings ###
+
+our @ISA       = qw(Exporter);
+our @EXPORT    = ();
+our @EXPORT_OK = qw(); # list routines that can be exported in other scripts
+our $VERSION   = 1.0;
+
+### Subroutines ###
+
+
