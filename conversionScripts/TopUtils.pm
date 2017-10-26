1f6485c46c2cae753366939f4f49b6c21481f024
MJLMills <mjohnmills@gmail.com>
2017-10-26 16:49:14 -0700

    correct extra comma and header

--- conversionScripts/TopUtils.pm
+++ conversionScripts/TopUtils.pm
@@ -1,6 +1,6 @@
-# Utilities Perl Module
+# TopUtils Perl Module
 # Dr. Matthew J L Mills - Rhorix v1.0 - June 2017
-# Subroutines for repeated tasks related to files and command-line arguments.
+# Subroutines for repeated tasks related to QCT
 
 package TopUtils;
 require Exporter;
@@ -9,7 +9,7 @@ require Exporter;
 
 our @ISA       = qw(Exporter);
 our @EXPORT    = ();
-our @EXPORT_OK = qw(getRank, getSignature);
+our @EXPORT_OK = qw(getRank getSignature);
 our $VERSION   = 1.0;
 
 ### Subroutines ###
