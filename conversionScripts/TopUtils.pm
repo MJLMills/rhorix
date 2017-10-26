70a4394e40aa196131e92975d27612f28fbd88f6
MJLMills <mjohnmills@gmail.com>
2017-10-26 16:46:52 -0700

    add getRank and getSignature routines

--- conversionScripts/TopUtils.pm
+++ conversionScripts/TopUtils.pm
@@ -9,9 +9,34 @@ require Exporter;
 
 our @ISA       = qw(Exporter);
 our @EXPORT    = ();
-our @EXPORT_OK = qw(); # list routines that can be exported in other scripts
+our @EXPORT_OK = qw(getRank, getSignature);
 our $VERSION   = 1.0;
 
 ### Subroutines ###
 
+sub getRank {
 
+  local $type = "$_[0]";
+
+  if ($type eq "bcp" or $type eq "rcp" or $type eq "ccp") {
+    return 3;
+  } else {
+    return 3;
+  }
+}
+
+sub getSignature {
+
+  local $type = "$_[0]";
+
+  if ($type eq "bcp") {
+    return -1;
+  } elsif ($type eq "rcp") {
+    return 1;
+  } elsif ($type eq "ccp") {
+    return 3;
+  } else {
+    return -3;
+  }
+
+}
