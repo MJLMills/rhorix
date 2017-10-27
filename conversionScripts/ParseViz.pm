ef09d85474d552d779b347a6512e09d53c4ad9e3
MJLMills <mjohnmills@gmail.com>
2017-10-26 18:09:50 -0700

    parse ring surfaces from mgpviz not iasviz

--- conversionScripts/ParseViz.pm
+++ conversionScripts/ParseViz.pm
@@ -25,8 +25,6 @@ sub parseMgpviz {
   # Read the gradient paths associated with CPs
   ($ails, $indices, $props) = parseMolecularGraphFromViz($_[0]);
 
-  ($ring_surface_gps, $ring_surface_indices, $ring_surface_props) = parseRingSurfacesFromMgpviz($_[0]);
-
   ($atomic_surface_coords,
    $atomic_surface_properties,
    $atomic_surface_indices,
@@ -39,6 +37,8 @@ sub parseMgpviz {
    $ring_surface_gps,
    $ring_surface_props,
    $ring_surface_indices) = parseRelatedIasvizFiles($elements,$nuclearIndices,$_[1]);
+
+  ($ring_surface_gps, $ring_surface_indices, $ring_surface_props) = parseRingSurfacesFromMgpviz($_[0]);
 
   return $sourceInformation,
          $elements,
