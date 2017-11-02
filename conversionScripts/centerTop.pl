9b743352bbbdc3cb358a5fdbedba5a5a578b8889
MJLMills <mjohnmills@gmail.com>
2017-11-01 17:53:18 -0700

    move getNucleiFromTop to ParseTop.pm

--- conversionScripts/centerTop.pl
+++ conversionScripts/centerTop.pl
@@ -5,6 +5,7 @@ use File::Basename;
 use lib dirname(__FILE__); # find modules in script directory - adds the path to @LIB
 use TopUtils qw(getMassesFromElements computeCOM);
 use Utilities qw(checkArgs readFile stripExt);
+use ParseTop qw(getNucleiFromTop);
 
 # Read the .top file (name passed as argument)
 $topFile = checkArgs(\@ARGV,"top");
@@ -44,46 +45,5 @@ foreach(@{$topContents}) {
   if ($edit == 0) {
     print TOP "$_\n";
   }
-
-}
-
-#*#*#* SUBROUTINES
-
-sub getNucleiFromTop {
-
-  my @topContents = @{$_[0]};
-
-  my @elements;
-  my @coordinates;
-
-  for ($topLine=0; $topLine<@topContents; $topLine++) {
-
-    if ($topContents[$topLine] =~ m/\<Nucleus\>/) {
-
-      for ($cpLine=$topLine; $cpLine<@topContents; $cpLine++) {
-
-        if ($topContents[$cpLine] =~ m/\<x\>(.*)\<\/x\>/) { $x = $1; }
-        if ($topContents[$cpLine] =~ m/\<y\>(.*)\<\/y\>/) { $y = $1; }
-        if ($topContents[$cpLine] =~ m/\<z\>(.*)\<\/z\>/) { $z = $1; }
-        if ($topContents[$cpLine] =~ m/\<element\>(.*)\<\/element\>/) { $element = $1; }
- 
-        if ($topContents[$cpLine] =~ m/\<\/Nucleus\>/) {
-
-          my @nuclear_coords = ($x, $y, $z);
-          push(@coordinates,\@nuclear_coords);
-          push(@elements,$element);
-
-          $topLine = $cpLine+1; # skip CP data
-          last; # go to next topoplogy file line
-
-        }
-
-      }
-
-    }
-
-  }
-
-  return \@elements, \@coordinates;
 
 }
