5235f0cd66d6ddec974279e5664670d2210525a9
MJLMills <mjohnmills@gmail.com>
2017-11-02 09:07:33 -0700

    move reformatsurface subroutine to ParseMif

--- conversion_scripts/ParseMif.pm
+++ conversion_scripts/ParseMif.pm
@@ -68,3 +68,87 @@ sub parseVertexLine {
 
 }
 
+sub reformatSurface {
+
+  my ($xPoints,$yPoints,$zPoints,$edgeA,$edgeB,$faceA,$faceB,$faceC) = @_;
+  my @isRedundant; 
+  my @xNew; 
+  my @yNew; 
+  my @zNew;
+
+  $start = time;
+
+  for ($i=0;$i<@$xPoints;$i++) {
+    $isRedundant[$i] = 0;
+  }
+
+  $n_x = @$xPoints; $n_y = @$yPoints; $n_z = @$zPoints;
+  if ($n_x == $n_y && $n_y == $n_z && $n_x != 0) {
+
+    $kept = 0;
+    CHECK_POINTS: for ($point=0;$point<@$xPoints;$point++) {
+
+      if ($isRedundant[$point] == 1) {
+        next CHECK_POINTS;
+      } else {
+        push(@xNew,@$xPoints[$point]); push(@yNew,@$yPoints[$point]); push(@zNew,@$zPoints[$point]);
+        $kept++; $rep = $kept - 1;
+
+        if ($printEdges == 1) {
+          CORRECT_EDGES: for ($j=$point;$j<@$xPoints;$j++) {
+
+#          if ($isRedundant[$j] == 1) { next CORRECT_EDGES; }
+
+            if (@$xPoints[$point] == @$xPoints[$j] && @$yPoints[$point] == @$yPoints[$j] && @$zPoints[$point] == @$zPoints[$j]) {
+
+              if ($point != $j) { $isRedundant[$j] = 1; }
+
+              for ($m=0;$m<@$edgeA;$m++) {
+                if (@$edgeA[$m] == $j) {
+                  @$edgeA[$m] = $rep;
+                } 
+                if (@$edgeB[$m] == $j) {
+                  @$edgeB[$m] = $rep;
+                }
+              }
+            }
+          }
+        }
+
+        CORRECT_FACES: for ($j=$point;$j<@$xPoints;$j++) {
+        
+          if (@$xPoints[$point] == @$xPoints[$j] && @$yPoints[$point] == @$yPoints[$j] && @$zPoints[$point] == @$zPoints[$j]) {
+
+            if ($point != $j) { $isRedundant[$j] = 1; }
+
+            for ($m=0;$m<@$faceA;$m++) {
+              if (@$faceA[$m] == $j) { @$faceA[$m] = $rep; }
+              if (@$faceB[$m] == $j) { @$faceB[$m] = $rep; }
+              if (@$faceC[$m] == $j) { @$faceC[$m] = $rep; }
+            }
+
+          }
+
+        }
+
+      }
+
+    }
+
+    $end = time; $elapsed = $end - $start; print "TOOK $elapsed SECONDS TO FIX SURFACE\t";
+
+    if ($n_x > 0) {
+      $n_redundant = 0;
+      for ($i=0;$i<@$xPoints;$i++) {
+        if ($isRedundant[$i] == 1) { $n_redundant++; };
+      } 
+      $percent = ($n_redundant / $n_x) * 100; $remains = @xNew;
+      printf "%d REDUNDANT POINTS \(OF $n_x\) REMOVED \(%5.2f percent\) LEAVING %d\n",$n_redundant,$percent,$remains;
+      printSurf(\@xNew, \@yNew, \@zNew, \@$edgeA, \@$edgeB, \@$faceA, \@$faceB, \@$faceC);
+    }
+
+  } else {
+    die "ERROR: MISMATCHED COORDINATE ARRAYS IN reformatSurface SUBROUTINE\n";
+  }
+
+}
