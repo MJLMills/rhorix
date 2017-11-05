1fa4e8770d23d10a260a5750df4b9996c979514d
MJLMills <mjohnmills@gmail.com>
2017-11-05 10:41:58 -0800

    remove old comments

--- conversion_scripts/mif2top.pl
+++ conversion_scripts/mif2top.pl
@@ -51,9 +51,6 @@ $indices,
 $props) = parseMif($mifContents,$factor,$removeRedundant,$printEdges);
 
 # for now ! These must come from parseMif instead for a complete script
-#$ails = [];
-#$indices = [];
-#$props = [];
 $atomic_surface_coords = []; 
 $atomic_surface_properties = [];
 $atomic_surface_indices = [];
