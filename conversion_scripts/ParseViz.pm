70a10760f7f95b845d3f4e82e4cfa0962e6b721e
MJLMills <mjohnmills@gmail.com>
2017-11-09 10:10:35 -0800

    correct empty cutoff in GP reader

--- conversion_scripts/ParseViz.pm
+++ conversion_scripts/ParseViz.pm
@@ -334,7 +334,7 @@ sub parseGradientPath {
   foreach(@{$_[0]}) {
 
     if ($_ =~ m/\s+(-?\d+\.\d+E[+-]\d+)\s+(-?\d+\.\d+E[+-]\d+)\s+(-?\d+\.\d+E[+-]\d+)\s+(-?\d+\.\d+E[+-]\d+)/) {
-      if ($4 >= 0.000) {
+      if (abs($4) >= 0.000) {
         my @coords = ($1,$2,$3);
         push(@points,\@coords);
 
